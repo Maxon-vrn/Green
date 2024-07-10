b8d9a94ea821871e19e80c43f31b0f3c5cfccf90
Maxim <fisenko_maxim@mail.ru>
2024-07-10 10:41:40 +0300

    start

---
+++ main.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+from fastapi import FastAPI,Request,Response
+from fastapi.templating import Jinja2Templates
+
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+
+
+class ModelName(str, Enum): #перечень страниц
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates/html")   #есть подозрения что не работает расширение и наследования шаблонизатора,тк он не понимает где файлы хранятся!!!
+app.mount("/templates", StaticFiles(directory="templates/html"), name="static")    #подключение директории для статических шаблонов
+app.mount("/images", StaticFiles(directory="templates/images"), name="images")
+
+dogs= [{'static': '1', 'templates': '2'}]
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
+
+@app.get('/')
+async def first_page(request: Request):
+    return templates.TemplateResponse("index.html",{'request':request})
+
+@app.get('/base')
+async def first_page():
+    return FileResponse("templates/html/base.html")
+
+
+@app.get('/contact')    #подключили джинжу для данных
+async def contact_page(request: Request):
+    return templates.TemplateResponse('contact.html', {"request": request,"name":"contact",'dogs': dogs})
